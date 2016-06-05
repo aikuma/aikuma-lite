4475a40e7f2b264f71142cbf2688e9f8fd27aff5
Sangyeop Lee <sangl1@student.unimelb.edu.au>
2016-06-06 02:25:54 +1000

    Re-order the injection of scripts

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ var gulp = require('gulp'),
 
 gulp.task('build', function () {
     // inject all of the js dependencies into the html
-    var sources = gulp.src(['./extdata/*.js', './src/**/*.js', './src/**/*.css', './directives/**/*.js'], {read: false});
+    var sources = gulp.src(['./extdata/*.js', './src/**/*.js', './src/**/*.css', './directives/app-core.js', './directives/*/**/*.js'], {read: false});
     return gulp.src('./src/window.html')
         .pipe(inject(sources, {addRootSlash: false}))
         .pipe(wiredep({ignorePath: '../'}))
